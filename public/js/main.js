c4001eafa5301aafcd47dc2f822396149c75413d
Glenn Hollingsworth <duelgamer@gmail.com>
2014-11-06 17:57:56 -0500

    Students being added to the queue

--- public/js/main.js
+++ public/js/main.js
@@ -1,139 +1,24 @@
 var socket = io();
 
-var numUsers;
 var myName = '';
-var selected = 0;
-var selectedRoles = {};
-var ready = false;
 
 $('form', '#join').submit(function(){
-  myName = $('#m').val();
+  myName = $('#name').val();
   if (myName != '') {
-    socket.emit('join game', myName);
+    socket.emit('join queue', myName);
     $('#join').hide();
-    $('#vote').show();
   }
   return false;
-});
-
-$('#readyBtn').click(function() {
-  if (selected == (numUsers + 3)) {
-    $('.roleSelect').each(function(i, obj) {
-      $(this).attr('disabled', 'disabled');
-    });
-    ready = true;
-    socket.emit('ready', ready, selectedRoles);
-    $('#readyBtn').hide();
-    $('#unreadyBtn').show();
-  } else {
-    ready = false;
-    socket.emit('ready', ready, selectedRoles);
-  }
-  return false;
-});
-
-$('#unreadyBtn').click(function() {
-  ready = false;
-  $('#readyBtn').show();
-  $('#unreadyBtn').hide();
-  $('.roleSelect').each(function(i, obj) {
-    if (obj.checked) {
-      $(this).removeAttr('disabled');
-    }
-  });
-  socket.emit('ready', ready, selectedRoles);
-  return false;
-});
-
-$('#startGame').click(function() {
-  document.getElementById("mp3").play();
-  socket.emit('start game');
-});
-
-$('input[class="roleSelect"]').change(function() {
-  if ($(this).context.checked) {
-    selected++;
-    selectedRoles[$(this).val()] = true;
-  } else {
-    selected--;
-    delete selectedRoles[$(this).val()];
-  }
-  updateRoles();
-});
-
-socket.on('name change', function(username) {
-  myName = username;
 });
 
 socket.on('update players', function(msg) {
-  numUsers = msg.numUsers;
-  $('span', '#vote').text("Vote for " + (numUsers + 3) + " roles:");
-  var allReady = true;
-  var isHost = msg.host == myName;
   $('#users').text('');
-  if (selected != (numUsers + 3) && ready) {
-    ready = false;
-    socket.emit('ready', ready, selectedRoles);
-  }
+  $('#students').text('There ' + (msg.numUsers == 1 ? 'is' : 'are') + ' currently ' + msg.numUsers + ' student' + (msg.numUsers == 1 ? '' : 's') + ' in the queue.')
+  var spot = 0;
   for (var key in msg.usernames) {
-    if (!msg.usernames[key].ready) {
-      allReady = false;
+    if (msg.usernames[key].username == myName) {
+      $('#mySpot').text("You are currently at position " + spot + " in the queue.");
     }
-    $('#users').append($('<li>').text(msg.usernames[key].username + (msg.usernames[key].ready ? " \u2713" : "") + (key == msg.host ? " \u2654" : "")));
+    spot++;
   }
-  if (isHost) {
-    $("#startGame").show();
-    if (allReady) {
-      $("#startGame").removeAttr('disabled');
-    } else {
-      $('#startGame').attr('disabled', 'disabled');
-    }
-  } else {
-    $("#startGame").hide();
-  }
-  updateRoles();
-});
-
-socket.on('hide vote', function() {
-  $('#vote').hide();
-});
-
-socket.on('roles in game', function(roles) {
-  $('#rolesInGame').show();
-  for (var idx in roles) {
-    $('#roles').append($('<li>').text(roles[idx]));
-  }
-});
-
-socket.on('player roles', function(playerRoles) {
-  $('#myRole').text("Your role: " + playerRoles[myName]);
-});
-
-function updateRoles() {
-  if (selected == (numUsers + 3)) {
-    $('#readyBtn').removeAttr('disabled');
-  } else {
-    $('#readyBtn').attr('disabled', 'disabled');
-    $('#readyBtn').show();
-    $('#unreadyBtn').hide();
-  }
-  $('.roleSelect').each(function(i, obj) {
-    if (selected > (numUsers + 3)) {
-      if (!obj.checked) {
-        $(this).attr('disabled', 'disabled');
-      } else {
-        $(this).removeAttr('disabled');
-      }
-    } else if (selected == (numUsers + 3)) {
-      if (!obj.checked) {
-        $(this).attr('disabled', 'disabled');
-      }
-    } else {
-      $(this).removeAttr('disabled');
-    }
-  });
-}
-
-socket.on('game in progress', function() {
-  $('#wrapper').hide();
 });
